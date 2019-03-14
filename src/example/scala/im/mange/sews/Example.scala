b473c780b0fb600667b4d115585363b0d569e79d
paulallton@gmail.com <goPfYgofPDD4teFYj>
2019-03-14 10:31:21 +0000

    make it a bit more like we are used to

--- src/example/scala/im/mange/sews/Example.scala
+++ src/example/scala/im/mange/sews/Example.scala
@@ -5,7 +5,7 @@ object Example extends App {
   final case class Color(red: Int, green: Int, blue: Int)
 
   sealed abstract class Shape extends Product with Serializable
-  final case class Circle(radius: Double, color: Color) extends Shape
+  final case class Circle(radius: Double, color: Color, width) extends Shape
   final case class Rectangle(width: Double, height: Double, color: Color) extends Shape
 
   import bridges.elm._
@@ -18,7 +18,10 @@ object Example extends App {
 //  import org.scalatest._
 //  import unindent._
 
+  case class All()
+
   private val decls = List(
+    decl[All],
     decl[Color],
     decl[Circle],
     decl[Rectangle],
