872a93be96e59d72aab0f800383841f8f9272335
paulallton@gmail.com <goPfYgofPDD4teFYj>
2019-04-05 16:25:55 +0100

    add the elm json pipeline stuff

---
+++ src/example/counter/Main.scala
@@ -0,0 +1,18 @@
+package counter
+
+object Main extends App {
+  import scala.sys.process._
+
+  //TODO: write Codec.elm
+  //TODO: write Main.html
+  //TODO: compile to elm.js instead
+  //TODO: do the port gubbins in the .html
+  //TODO: add the pipeline/decoder stuff that bridges needs
+
+  //TODO: run this in currently working directory
+  "./build.sh".!<
+
+
+  //TODO: docs, point to https://github.com/davegurnell/bridges
+  //see: https://github.com/NoRedInk/elm-decode-pipeline -> https://github.com/NoRedInk/elm-json-decode-pipeline
+}
